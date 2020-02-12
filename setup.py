ecb0ab3051413e339342a7bb3407f7b9156b179b
mohamed-ali <m.ali.jamaoui@gmail.com>
2020-02-12 22:29:40 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,16 @@ import hadith
 setup(
     name='hadith',
     version=hadith.__version__,
+    author="Umma Open Source",
+    author_email="umma.opensource@gmail.com",
     description='Helps hadith scholars with quantitative research',
+    url="https://github.com/Umma1/hadith/",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=['pandas'],
     python_requires='>=3.6',
     include_package_data=True
