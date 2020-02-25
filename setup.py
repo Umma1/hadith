f948a1676bc04c4331d402c58f86fd838645f6d3
mohamed-ali <m.ali.jamaoui@gmail.com>
2020-02-25 21:22:27 +0100

    Add pyyaml as a dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['pandas'],
+    install_requires=['pandas', 'pyyaml'],
     python_requires='>=3.6',
     include_package_data=True
 )
