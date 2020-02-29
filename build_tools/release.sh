97f5828ea7b320f5e870b99383e67bb41deadac9
mohamed-ali <m.ali.jamaoui@gmail.com>
2020-02-29 22:06:34 +0100

    تحيين البرنامج و تصحيحات مختلفة

--- build_tools/release.sh
+++ build_tools/release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python3 -m pip install --user --upgrade setuptools wheel
+python3 -m pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/*
 rm -r build/
